7d70aaaf0fee5cc6875639ba9d8cee7f286732f7
loski82 <carlos.ramia@careerbuilder.com>
2015-11-06 02:43:51 +0000

    Completed In Class App

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151105232124) do
+ActiveRecord::Schema.define(version: 20151106023557) do
+
+  create_table "categories", force: :cascade do |t|
+    t.string   "name"
+    t.integer  "make_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "makes", force: :cascade do |t|
     t.string   "name"
